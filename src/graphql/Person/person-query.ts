71fee812c1a83747e1762742da812a1d6862a537
Franciszek Stodulski <f.stodulski@majesticdev.co>
2019-06-18 11:31:05 +0200

    feat: added avatarURL for person, added new Query for Person (#3)
    * feat: added avatarURL for person, added new Query for Person
    
    * refactor: avatar_url

--- src/graphql/Person/person-query.ts
+++ src/graphql/Person/person-query.ts
@@ -3,4 +3,13 @@ import { Context } from '@interface/prisma';
 export const personQuery = {
   getAllPersons: async (parent, args, { prisma: { persons } }: Context) =>
     await persons(),
+
+  getPersonsByJobID: async (parent, { ID }, { prisma: { persons } }: Context) =>
+    await persons({
+      where: {
+        job: {
+          id: ID,
+        },
+      },
+    }),
 };
