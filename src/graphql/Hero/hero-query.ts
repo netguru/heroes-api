5b574c7247f1194ee045203aa934b1defd4cada4
Franciszek Stodulski <franciszek.stodulski@netguru.pl>
2019-06-26 14:22:53 +0200

    refactor: changed a little bit

--- src/graphql/Hero/hero-query.ts
+++ src/graphql/Hero/hero-query.ts
@@ -6,23 +6,14 @@ export const heroQuery = {
     { type_id, name_query },
     { prisma: { heroes } }: Context,
   ) => {
-    if (type_id) {
-      return await heroes({
-        where: {
-          type: {
-            id: type_id,
-          },
+    return await heroes({
+      where: {
+        full_name_contains: name_query,
+        type: {
+          id: type_id,
         },
-      });
-    }
-
-    if (name_query) {
-      return await heroes({
-        where: {
-          full_name_contains: name_query,
-        },
-      });
-    }
+      },
+    });
 
     return await heroes();
   },
