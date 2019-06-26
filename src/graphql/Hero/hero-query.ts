1de14a997685a575d57aba36af30d6e937e9dae0
Franciszek Stodulski <f.stodulski@majesticdev.co>
2019-06-26 14:26:13 +0200

    feat: add search api (#10)
    * feat: add search api
    
    * refactor: changed a little bit

--- src/graphql/Hero/hero-query.ts
+++ src/graphql/Hero/hero-query.ts
@@ -1,17 +1,20 @@
 import { Context } from '@interface/prisma';
 
 export const heroQuery = {
-  heroes: async (parent, { id }, { prisma: { heroes } }: Context) => {
-    if (!id) {
-      return await heroes();
-    }
-
+  heroes: async (
+    parent,
+    { type_id, name_query },
+    { prisma: { heroes } }: Context,
+  ) => {
     return await heroes({
       where: {
+        full_name_contains: name_query,
         type: {
-          id,
+          id: type_id,
         },
       },
     });
+
+    return await heroes();
   },
 };
