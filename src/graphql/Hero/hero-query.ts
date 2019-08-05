3db1bf8a8dc59efb395d7b120d88f087302310ca
Franciszek Stodulski <f.stodulski@majesticdev.co>
2019-08-05 12:37:33 +0200

    feat: heroes pagination (#16)

--- src/graphql/Hero/hero-query.ts
+++ src/graphql/Hero/hero-query.ts
@@ -3,25 +3,24 @@ import { Context } from '@interface/prisma';
 export const heroQuery = {
   heroes: async (
     parent,
-    { type_id, name_query },
+    { type_id, name_query, skip, first },
     { prisma: { heroes } }: Context,
   ) => {
     return await heroes({
       where: {
-        full_name_contains: name_query,        
+        full_name_contains: name_query,
+
         type: {
           id: type_id,
-        }  
+        },
       },
+      skip,
+      first,
     });
   },
-  hero: async (
-    parent,
-    { id },
-    { prisma: { hero } }: Context,
-  ) => {
+  hero: async (parent, { id }, { prisma: { hero } }: Context) => {
     return await hero({
-      id
+      id,
     });
   },
 };
