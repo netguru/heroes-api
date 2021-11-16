c067d76753abdd5be08aeac96e29da68187576c7
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2021-11-16 16:16:35 +0100

    feat: import all submodules in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { HeroesModule } from './heroes';
 import { ConfigModule } from './config';
 import { DatabaseModule } from './database';
+import { AvatarsModule } from './avatars';
+import { TypesModule } from './types';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, HeroesModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    HeroesModule,
+    AvatarsModule,
+    TypesModule,
+  ],
 })
 export class AppModule {}
