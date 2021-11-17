8140aa91377f2037ac93132a878d2dc6cd275356
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2021-11-17 17:53:07 +0100

    feat: api paginated response decorator for swagger

---
+++ src/decorators/ApiPaginatedResponse.ts
@@ -0,0 +1,22 @@
+import { applyDecorators, Type } from '@nestjs/common';
+import { ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+import { PaginatedDto } from '../dtos';
+
+export const ApiPaginatedResponse = <TModel extends Type>(model: TModel) =>
+  applyDecorators(
+    ApiOkResponse({
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(PaginatedDto) },
+          {
+            properties: {
+              results: {
+                type: 'array',
+                items: { $ref: getSchemaPath(model) },
+              },
+            },
+          },
+        ],
+      },
+    }),
+  );
